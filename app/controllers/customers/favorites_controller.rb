2cb7cc87e3e8132d294fabf90ca0cbca91fc63e6
okadareo0614@gmail.com <okadareo0614@gmail.com>
2021-02-19 17:33:18 +0000

    ヘッターレイアウト調整、検索機能調整

--- app/controllers/customers/favorites_controller.rb
+++ app/controllers/customers/favorites_controller.rb
@@ -4,13 +4,11 @@ class Customers::FavoritesController < ApplicationController
     estate = Estate.find(params[:estate_id])
     favorite = current_customer.favorites.new(estate_id: estate.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     estate = Estate.find(params[:estate_id])
     favorite = current_customer.favorites.find_by(estate_id: estate.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
