a3887924d33dd507694286b16e107c236328c3a3
okadareo0614@gmail.com <okadareo0614@gmail.com>
2021-03-29 03:20:56 +0000

    カレンダーの日付の開始日時と終了日時のバリデーション追加

--- app/controllers/customers/favorites_controller.rb
+++ app/controllers/customers/favorites_controller.rb
@@ -13,10 +13,8 @@ class Customers::FavoritesController < ApplicationController
   def destroy
     @estate = Estate.find(params[:estate_id])
     @favorite = current_customer.favorites.find_by(estate_id: @estate.id)
+    # @favorite = Favorite.find_by(estate_id: params[:estate_id], customer_id: current_customer.id)
+    # 16行目は15行目を細かく書いたもの。params[:estate_id]は14行目の変数を代入して(@estate.id)でも可能
     @favorite.destroy
-    respond_to do |format|
-      format.html { redirect_to customers_path }
-      format.js
-    end
   end
 end
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,7 +2,15 @@ class Calendar < ApplicationRecord
   belongs_to :admin
 
   validates :title,  presence: true
-  validates :body,  presence: true
   validates :start_time,  presence: true
   validates :finish_time,  presence: true
+  validate :start_finish_check
+
+  def start_finish_check
+    return if start_time.blank? && finish_time.blank?
+
+    if start_time > finish_time
+      errors.add(:finish_time, "が開始日時より前の日付では登録できません。")
+    end
+  end
 end
